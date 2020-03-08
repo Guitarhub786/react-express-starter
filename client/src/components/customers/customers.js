4fcde777ba3400ccfcc87df116a79dfc4338f525
Guitarhub786 <codewithdrawal@gmail.com>
2020-03-08 20:00:03 +0000

    Customer Component Class Version

--- client/src/components/customers/customers.js
+++ client/src/components/customers/customers.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './customers.css';
 
-function Customers() {
-  return (
-    <div>
-      <h2>Customers</h2>
-    </div>
-  );
+class Customers extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Customers Class</h2>
+      </div>
+    );
+  }
 }
 
 export default Customers;
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import './customers.css';
+
+// function Customers() {
+//   return (
+//     <div>
+//       <h2>Customers</h2>
+//     </div>
+//   );
+// }
+
+// export default Customers;
